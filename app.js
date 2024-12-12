292538d399fcf7279110603d1445f5693be81426
Aundre Joseph <josephaundre72@@gmail.com>
2024-12-11 19:00:53 -0800

    start

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const port = 3000
-app.listen(port, () => {
-  console.log(` server listening on port ${port} `)
-})
+
+const dotenv=require("dotenv")
+const express = require("express");
+dotenv.config()
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT;
+const enviroment = process.env.enviroment;
+
+app.get("/hello", (reguest, response) => {
+  console.log(`the port in use is: ${PORT} `);
+
+  response.send("Hello World");
+});
+
+app.get("/status", (request, response) => {
+  return response.status(200).json({
+    port: PORT,
+    enviroment: enviroment,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(` server listening on port ${PORT} `);
+});
---
+++ readme.md
@@ -0,0 +1,10 @@
+node.js-  a JavaScript runtime environment that allows developers to execute JavaScript code on the server side, outside of a web browser,Allows v8 a java script engine to run on your machine 
+
+Express- a minimal and flexible web application framework for Node.js. 
+
+
+3- High Performance-Non-Blocking, Asynchronous I/O,Rich Ecosystem with npm,Single-Threaded Event Loop
+
+flexibility-Extensive Ecosystem,JavaScript Everywhere,Real-Time Communication,Microservices and Serverless Architectures,Non-Blocking, Asynchronous Architecture
+
+capabilties-Node.js supports various testing frameworks like Mocha, Jest, and Jasmine, making it easy to write unit tests and integrate automated testing into the development workflow, File System Interaction
